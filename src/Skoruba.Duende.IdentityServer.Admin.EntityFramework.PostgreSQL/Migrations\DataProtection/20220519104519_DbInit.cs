2e56e800858e6e5f7c10e96a17d994a58190096f
hyounoo <9585318+hyounoo@users.noreply.github.com>
2022-05-20 14:07:31 +0900

    startup

---
+++ src/Skoruba.Duende.IdentityServer.Admin.EntityFramework.PostgreSQL/Migrations\DataProtection/20220519104519_DbInit.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Skoruba.Duende.IdentityServer.Admin.EntityFramework.PostgreSQL.MigrationsDataProtection
+{
+    public partial class DbInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
